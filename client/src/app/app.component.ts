f795c93f5d24b91a845a24b217bed191545d0a0f
Hao Bui <iamhaobui@gmail.com>
2018-11-29 17:09:06 -0500

    remove unneccessary

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Video, VIDEOS, VIDEO_HEADERS } from './videos';
 import { Customer, CUSTOMERS, CUSTOMER_HEADERS } from './customers';
-import { AuthService} from './services/auth.service';
-import { Router} from '@angular/router';
-import { FlashMessagesService} from 'angular2-flash-messages';
 
 
 @Component({
@@ -18,20 +15,4 @@ export class AppComponent {
 
   customers = CUSTOMERS;
   customer_header = CUSTOMER_HEADERS;
-
-  constructor(
-    public authService: AuthService,
-    private router: Router,
-    private flashMessage: FlashMessagesService
-  ) {}
-
-  onLogoutClick() {
-    this.authService.logout();
-    this.flashMessage.show('You are logged out', {
-      cssClass: 'alert-success',
-      timeout: 3000
-    });
-    this.router.navigate(['/admin/login']);
-    return false;
-  }
 }
