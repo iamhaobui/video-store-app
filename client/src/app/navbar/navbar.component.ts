674250aaf113c879c9435c515c79278a211f2c0c
Hao Bui <iamhaobui@gmail.com>
2018-11-29 17:05:20 -0500

    add event onKey() for flitering

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AdminLoginService } from '../services/admin-login.service';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { AuthService} from '../services/auth.service';
-// import { Router} from '@angular/router';
 import { FlashMessagesService} from 'angular2-flash-messages';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,13 +12,17 @@ import { FlashMessagesService} from 'angular2-flash-messages';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @Output() outputSearchText = new EventEmitter<string>();
+
+  searchText: string;
   token: any;
   username: string;
 
   constructor(
       public authService: AuthService,
       private router: Router,
-      private flashMessage: FlashMessagesService
+      private flashMessage: FlashMessagesService,
+      private dataService: DataService,
     ) {}
 
   onLogoutClick() {
@@ -30,34 +34,24 @@ export class NavbarComponent implements OnInit {
     this.router.navigate(['/admin/login']);
     return false;
   }
-  // constructor(
-  //   private adminLoginService: AdminLoginService,
-  //   private router: Router,
-  //   private location: Location,
-  // ) { }
-
-  // onLogout() {
-  //   this.adminLoginService.logoutAdmin();
-  //   this.router.navigate(['/']);
-  //   this.ngOnInit();
-  // }
   ngOnInit() {
     this.token = localStorage.getItem('id_token');
-    this.username = JSON.parse(localStorage.getItem('user')).name;
+    this.username = JSON.parse(localStorage.getItem('admin')).username;
+    this.dataService.currentText.subscribe(searchText => this.searchText = searchText);
   }
   onLogin() {
     this.router.navigate(['/admin/login']);
   }
-  // onRegister() {
-  //   this.adminLoginService.logoutAdmin();
-  //   this.router.navigate(['/']);
-  // }
+
   isLoggedIn() {
     if (this.token == null) {
       return false;
     } else {
       return true;
     }
+  }
+  onKey(event: any) {
+    this.dataService.changeText(event.target.value);
   }
 
 }
