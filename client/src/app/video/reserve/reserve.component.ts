fc325656e2ee80475c1c36b1608e7726ca9d85a0
Tam Dang <danhtam0509@gmail.com>
2018-11-30 15:31:26 -0500

    get id from paramMap then call getVideo service to assign data to properties

--- client/src/app/video/reserve/reserve.component.ts
+++ client/src/app/video/reserve/reserve.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { VIDEOS, VIDEO_HEADERS } from 'src/app/videos';
 import { Location } from '@angular/common';
+import { ActivatedRoute} from '@angular/router';
 import { Router } from '@angular/router';
+import { FlashMessagesService} from 'angular2-flash-messages';
+import { VideoService} from '../../services/video.service';
+
 
 @Component({
   selector: 'app-reserve',
@@ -19,12 +23,29 @@ export class ReserveComponent implements OnInit {
   director: string;
   status: string; 
 
+  private id = this.route.snapshot.paramMap.get('id');
+
+
  constructor(
     private router: Router,
     private location: Location,
+    private route: ActivatedRoute, 
+    private flashMessage: FlashMessagesService,
+    private videoService: VideoService
   ) { }
 
   ngOnInit() {
+    this.videoService.getVideo(this.id)
+      .subscribe(video => {
+        console.log(video);
+        this.title = video.title;
+        this.runningTime = video.runningTime;
+        this.genre = video.genre;
+        this.rating = video.rating;
+        this.director = video.director;
+        this.status = video.status;
+      }
+      )
   }
   goBack() {
     this.location.back();
